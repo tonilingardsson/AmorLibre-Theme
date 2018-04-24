7c515416eaa8e9900cc835034310544e427d9986
tonilingardsson <toni.lingardsson@gmail.com>
2018-04-24 22:00:03 +0200

    Creating files to build up WP
    I create files with what I think is essential php code to see some
    progress in my theme. Now is just “Hello World!” in a h1.

--- wp-content/themes/amorlibre/page.php
+++ wp-content/themes/amorlibre/page.php
@@ -6,3 +6,12 @@
 */
 get_header();
 ?>
+<div class="wrapp"> WRAPP
+<div id="primary" class="content-area">CONTENT-AREA
+    <main id="main" class="site-main" role="main">
+    <p>Insert a WHILE here to pick up different template parts and display them on the page.</p>
+</main>
+</div>
+</div>
+<?php
+get_footer();
---
+++ wp-content/themes/amorlibre/search.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package WordPress
+ */
+get_header();
+?>
+
+<div class="wrapp">
+    <header clas="page-header">
+        If statement here for have_posts()
+        <h1 class="page-title">Search function in php.
+        </h1>
+    </header>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            Another if statement in php. And then a while.<p>Sorry, but no matches.</p>
+        </main>
+    </div>
+<?php get_sidebar();
+?>
+</div>
+
+<?php
+get_footer();
