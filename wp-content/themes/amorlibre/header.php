def9fc7e7e0371241320eef0de3c15338934335b
tonilingardsson <toni.lingardsson@gmail.com>
2018-05-23 09:44:54 +0200

    Changes. Style and responsivity
    I have change the color of the check-out button, the display of the shop-page for the products and I made the theme resposive to all devices.

--- wp-content/themes/amorlibre/header.php
+++ wp-content/themes/amorlibre/header.php
@@ -13,6 +13,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
